fc8588a21378d345c1b270544902eddebac83ef2
Sakshi2929 <77919823+Sakshi2929@users.noreply.github.com>
2022-01-20 01:27:43 +0530

    inc folder

---
+++ inc/footer.php
@@ -0,0 +1,64 @@
+  <footer class="footer-2">
+    <div class="container">
+      <div class="row">
+
+        <!-- Support -->
+        <div class="col-sm-7">
+          <h6>About Us</h6>
+          <div class="about-foot">
+            <p>
+              Health Assistant is a web service in which the proposed system tries to eliminate users’ need to figure out their disease by giving them access to a centralized clinical repository in a much interactive manner. Users can also ask questions regarding their disease and even book online and offline appointments with doctors.Our Webservice stores the Health record of the user on a remote server. User can enter the Symptoms or Disease and Our System try to figure out
+            </p>
+          </div>
+        </div>
+
+
+
+        <!-- Conatct -->
+        <div class="col-sm-5">
+          <div class="con-info">
+            <h6>Contact Us</h6>
+            <p>402 Neev Chawk, New Delhi</p>
+            <ul>
+              <li class="mobile">
+                <p>9110 800 6054</p>
+                <p>9-269-690-VA</p>
+              </li>
+              <li class="email">
+                <p>vamedscape@gmail.com</p>
+                <p>info@deskphp.com</p>              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Rights -->
+  <div class="rights style-2">
+    <div class="container">
+      <p>© <?php echo date("Y"); ?> Virtual Assistant Developed By <span style="color: white">Fruits of Binary Tree</span></p>
+    </div>
+  </div>
+
+  <!-- GO TO TOP  -->
+  <a href="#" class="cd-top"><i class="fa fa-angle-up"></i></a>
+  <!-- GO TO TOP  -->
+</div>
+<!-- End Page Wrapper -->
+
+<!-- JavaScripts -->
+<script src="js/vendors/jquery/jquery.min.js"></script>
+<script src="js/vendors/wow.min.js"></script>
+<script src="js/vendors/bootstrap.min.js"></script>
+<script src="js/vendors/own-menu.js"></script>
+<script src="js/vendors/jquery.sticky.js"></script>
+<script src="js/vendors/owl.carousel.min.js"></script>
+<script src="js/vendors/jquery.magnific-popup.min.js"></script>
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.t.min.js"></script>
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
