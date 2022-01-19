6c49b0f806a7e0482c50668a52637a1fde28d8ae
Ekta12-eng <62852379+Ekta12-eng@users.noreply.github.com>
2022-01-20 01:27:59 +0530

    index.php added

---
+++ index.php
@@ -0,0 +1,338 @@
+
+  <?php include 'header.php';?>
+  <!-- Bnr Header -->
+  <section class="main-banner">
+    <div class="tp-banner-container">
+      <div class="tp-banner">
+        <ul>
+          
+          <!-- SLIDE  -->
+          <li data-transition="random" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" > 
+            <!-- MAIN IMAGE --> 
+            <img src="images/hms.jpg"  alt="slider"  data-bgposition=" center center" data-bgfit="cover" data-bgrepeat="no-repeat"> 
+            
+            <!-- LAYER NR. 1 -->
+            <div class="tp-caption sfl tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-120" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300"
+                style="z-index: 5; font-size:50px; font-weight:500; color:black;  max-width: auto; max-height: auto; white-space: nowrap;">Virtual Health assistant </div>
+            
+            <!-- LAYER NR. 2 -->
+            <div class="tp-caption sfr tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-60" 
+                data-speed="800" 
+                data-start="1000" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                style="z-index: 6; font-size:40px; color:gray; font-weight:500; white-space: nowrap;">We care about your health </div>
+            
+            <!-- LAYER NR. 3 -->
+            <div class="tp-caption sfb tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="0" 
+                data-speed="800" 
+                data-start="1200" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="none" 
+                data-splitout="none" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                style="z-index: 7;  font-size:22px; color:gray; font-weight:500; max-width: auto; max-height: auto; white-space: nowrap;">Your trust matters to this website </div>
+            
+            <!-- LAYER NR. 4 -->
+            <div class="tp-caption lfb tp-resizeme scroll" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="120"
+                data-speed="800" 
+                data-start="1300"
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 8;"><a href="appointment.php" class="btn">Register here</a> </div>
+          </li>
+          
+          <!-- SLIDE  -->
+          <li data-transition="random" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" > 
+            <!-- MAIN IMAGE --> 
+            <img src="images/hms2.jpg"  alt="slider"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat"> 
+            <!-- LAYER NR. 1 -->
+            <div class="tp-caption sfl tp-resizeme" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="-100" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300"
+                style="z-index: 5; font-size:40px; font-weight:500; color:#000;  max-width: auto; max-height: auto; white-space: nowrap;">Best Virtual Assistant</div>
+            
+            <!-- LAYER NR. 2 -->
+            <div class="tp-caption sfr tp-resizeme" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="-40" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="chars" 
+                data-splitout="none" 
+                data-elementdelay="0.03" 
+                data-endelementdelay="0.4" 
+                data-endspeed="300"
+                style="z-index: 5; font-size:55px; font-weight:500; color:#000;  max-width: auto; max-height: auto; white-space: nowrap;">Care And Cure</div>
+            
+            <!-- LAYER NR. 3 -->
+            <div class="tp-caption sfb tp-resizeme" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="30" 
+                data-speed="800" 
+                data-start="1400" 
+                data-easing="Power3.easeInOut" 
+                data-splitin="none" 
+                data-splitout="none" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                style="z-index: 7; font-size:16px; color:#000; font-weight:500; line-height:26px; max-width: auto; max-height: auto; white-space: nowrap;">Improved diagnostic performance and heightened satisfaction of patients <br> and physicians delight.</div>
+            
+            <!-- LAYER NR. 4 -->
+            <div class="tp-caption lfb tp-resizeme scroll" 
+                data-x="left" data-hoffset="400" 
+                data-y="center" data-voffset="140"
+                data-speed="800" 
+                data-start="1600"
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 8;"><a href="#." class="btn">CONTACT NOW</a> </div>
+          </li>
+          
+          <!-- SLIDE  -->
+          <li data-transition="random" data-slotamount="7" data-masterspeed="300"  data-saveperformance="off" > 
+            <!-- MAIN IMAGE --> 
+            <img src="images/hms3.jpg"  alt="slider"  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat"> 
+            
+            <!-- LAYER NR. 2 -->
+            <div class="tp-caption sfb tp-resizeme" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-80" 
+                data-speed="800" 
+                data-start="800" 
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 6; font-size:40px; color:#000; font-weight:500; white-space: nowrap;"> Welcome To Our Medscape Website  </div>
+            
+            <!-- LAYER NR. 3 -->
+            <div class="tp-caption sfb tp-resizeme text-center" 
+                data-x="center" data-hoffset="0" 
+                data-y="center" data-voffset="-10" 
+                data-speed="800" 
+                data-start="1000" 
+                data-easing="Power3.easeInOut" 
+                data-elementdelay="0.1" 
+                data-endelementdelay="0.1" 
+                data-endspeed="300" 
+                data-scrolloffset="0"
+                style="z-index: 7; font-size:20px; font-weight:500; line-height:26px; color:#000; max-width: auto; max-height: auto; white-space: nowrap;">We work in a friendly and efficient using the latest <br>
+              technologies and sharing our expertise.</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  
+  <!-- Content -->
+  <div id="content"> 
+    
+    <!-- Intro -->
+    <section class="p-t-b-150">
+      <div class="container">
+        <div class="intro-main">
+          <div class="row"> 
+            
+            <!-- Intro Detail -->
+            <div class="col-md-8">
+              <div class="text-sec">
+                <ul class="row">
+                  <li class="col-sm-6">
+                    <h5> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h5>
+                    <h4>Run to the doctor</h4>
+                    <p>Details to the best hospitals to visit :-
+                      <br>
+                      <ul>
+                       
+                      <a href="https://platinumhospitals.in/"  target="_blank">  Platinum Hospital</a>
+                      <br>
+                      <a href="https://www.hindujahospital.com/"  target="_blank">Hinduja Hospital</a>
+                      <br>
+                      <a href="http://www.ltmgh.com/frontview/index.aspx"  target="_blank"> Lokmanya Tilak Municipal General Hospital</a>
+                      <br>
+                      <a href="https://www.kokilabenhospital.com/professionals/vijaychandramaniya.html"  target="_blank"> Dr. Vijay Chandramaniya - Kokilaben Hospital</a>
+                    </p>
+                  </li>
+                </ul>
+                <li>
+                  <li class="col-sm-6">
+                    <h5> <i class="lnr  lnr-checkmark-circle block-header"></i>Online Doctor Consultations<h4> For Covid Patients</h4>
+                      <iframe width="300" height="200" src="https://www.youtube.com/embed/NzPpWTMxPpI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </li>
+                  <li class="col-sm-6">
+              <div class="timing"> <i class="lnr lnr-clock"></i>
+                     < <i class="lnr lnr-checkmark-circle"></i>
+
+                     <!-- </i><h5 style="color:antiquewhite" margin="20px" padding="90px">Top doctors List</h5> --> 
+                    <p ><ul>
+                      <li style="color:antiquewhite" >
+                      Top doctors List
+                      </li>
+                      <li>
+                        
+                      Carol Bosworth</li>
+                      <li>
+                      Sirena S Rivera
+                      </li>
+                      <li>
+                      Will Williams
+                      </li>
+                      
+                    </ul></p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h5>  <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                    <p> Counseling provides a safe haven for precisely that kind of free-ranging release: You can say things in the therapist's office, with the therapist present, that would be incendiary or hurtful in your living room.
+                     <br> Get Your Couseling for free-here <strong> +91 6453624276<br></strong></p>
+                  </li>
+              </div>
+            </div>
+</li>
+            <!-- Intro Timing -->
+            <div class="col-md-4">
+              <div style: background-color="blue" class="background-health">
+                  <img src="../hospital-management/images/doctorrequired.jpg"alt="">
+                  <!-- <li> Monday <span>8.00 - 16.00</span></li>
+                  <li> Tuesday <span>8.00 - 16.00</span></li>
+                  <li> Wednesday <span>8.00 - 16.00</span></li>
+                  <li> Thursday <span>8.00 - 16.00</span></li>
+                  <li> Friday <span>8.00 - 16.00</span></li>
+                  <li> Saturday <span>8.00 - 16.00</span></li>
+                  <li> Sunday <span>8.00 - 16.00</span></li>
+                </ul> -->
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+    </section>
+    <!-- DOCTOR LIST -->
+    <section class="p-t-b-150">
+      <div class="container">
+
+        <!-- Heading -->
+        <div class="heading-block">
+          <h2>Our Services</h2>
+          <hr>
+          <span>Backed by a strong doctors we continue to innovate with newer services and enhanced customer experience virtually.</span> </div>
+
+        <!-- Services -->
+        <div class="services">
+          <div class="row">
+
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-eye-2 icon"></i> </div>
+                <div class="media-body">
+                  <h6>Eye Specialist</h6>
+                  <p>The retina covers the back of your eye, and is responsible for receiving light and sending those signals to your brain.</p>
+                </div>
+              </article>
+            </div>
+
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-operating-room icon"></i> </div>
+                <div class="media-body">
+                  <h6>Children Specialist</h6>
+                  <p>We believe that even your healthy children need preventive health services during their growth and evolution.</p>
+                </div>
+              </article>
+            </div>
+
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-icu-monitor icon"></i> </div>
+                <div class="media-body">
+                  <h6>Psychologist</h6>
+                  <p>Ready to take the first step? Our skilled counselors are ready to listen and guide you through healing.We help clients to work through anxiety,,life transitions,and more.</p>
+                </div>
+              </article>
+            </div>
+
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-doctor icon"></i> </div>
+                <div class="media-body">
+                  <h6>Emergency</h6>
+                  <p>We provide those are in need.This is mainly for those who ae near us.Call for help at the number given above.</p>
+                </div>
+              </article>
+            </div>
+
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-heartbeat icon"></i> </div>
+                <div class="media-body">
+                  <h6>Heart Problems</h6>
+                  <p>With a variety of services ranging from preventative care for those at risk for heart disease to treatment for those experiencing the first signs of a problem, such as a heart attack or abnormal heart rhythm, we’ll be with you to make sure things are under control.</p>
+                </div>
+              </article>
+            </div>
+
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-stomach-2 icon"></i> </div>
+                <div class="media-body">
+                  <h6>Stomach Problems</h6>
+                  <p>Our services can heal your stomach.We provide best consultation from experienced doctors.</p>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+   
+    
+    
+  </div>
+  
+  <!-- Footer -->
+<?php include 'footer.php';?>
