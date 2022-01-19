c31df09ce5ad3ddf914fd06d5b4f9f05376c11a3
Ekta12-eng <62852379+Ekta12-eng@users.noreply.github.com>
2022-01-20 04:47:02 +0530

    about updated

--- about.php
+++ about.php
@@ -21,25 +21,33 @@
                             <ul class="row">
                                 <li class="col-sm-6">
                                     <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
-                                    <p>Excepteur sint occaecat cupidatat non roident,
-                                        sunt in culpa qui officia deserunt mollit </p>
+                                    <p>Find A Doctor: Emergency Medicine Doctors in India
+Find the right health specialist from our list of doctors. You can also search by their name to narrow down the list</p>
+                       <a href="https://platinumhospitals.in/"  target="_blank">  Platinum Hospital</a>
+                       <br>
+                       <a href="https://www.hindujahospital.com/"  target="_blank">Hinduja Hospital</a>
+                       <br>
+                       <a href="http://www.ltmgh.com/frontview/index.aspx"  target="_blank"> Lokmanya Tilak Municipal General Hospital</a>
+                       <br>
+                       <a href="https://www.kokilabenhospital.com/professionals/vijaychandramaniya.html"  target="_blank"> Dr. Vijay Chandramaniya - Kokilaben Hospital</a>
+                     
                                 </li>
                                 <li class="col-sm-6">
                                     <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
-                                    <p>Excepteur sint occaecat cupidatat non roident,
-                                        sunt in culpa qui officia deserunt mollit </p>
+                                    There are only two sorts of doctors: those who practice with their brains, and those who practice with their tongues." ...
+"The aim of medicine is to prevent disease and prolong life, the ideal of medicine is to eliminate the need of a physician."
 </li>
                                 <li class="col-sm-6">
                                     <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
-                                    <p>Excepteur sint occaecat cupidatat non roident,
-                                        sunt in culpa qui officia deserunt mollit </p>
+                                    <p>Counseling provides a safe haven for precisely that kind of free-ranging release: You can say things in the therapist's office, with the therapist present, that would be incendiary or hurtful in your living room.
+                     <br> Get Your Couseling for free-here <strong> +91 6453624276<br></strong></p></p>
                                 </li>
                             </ul>
                         </div>
                     </div>
 
                     <!-- Intro Timing -->
-                    <div class="col-md-5"> <img class="img-responsive intro-img" src="images/intro-img.jpg" alt="">
+                    <div class="col-md-5"> <img class="img-responsive intro-img" width="1000px" height="200px"src="https://us.123rf.com/450wm/creativebucket/creativebucket1911/creativebucket191100704/133155714-dermatologist-doctor-double-thumbs-up-vector.jpg?ver=6" alt="">
                     </div>
                 </div>
             </div>
